a0a53a5c6cbbcb9c4b109d41a87834ac52d38926
unknown <alaminkhan6203@gmail.com>
2023-02-12 15:39:54 +0600

    first request done

--- components/Appointments/AvailableAppointments/AppointmentCards.tsx
+++ components/Appointments/AvailableAppointments/AppointmentCards.tsx
@@ -1,49 +1,64 @@
-import { format } from 'date-fns'
-import React from 'react'
-import {useState,useEffect} from 'react'
-import AppointmentSlot from './AppointmentSlot'
+import { format } from "date-fns";
+import React from "react";
+import { useState, useEffect } from "react";
+import AppointmentSlot from "./AppointmentSlot";
+import axios from "axios";
 
- export interface DateProps{
-    selectedDate: Date | undefined
+export interface DateProps {
+  selectedDate: Date | undefined;
 }
-export interface AppointmentsProps{
-    id:string,
-    name:string,
-    slots: string[]
+export interface AppointmentsProps {
+  id: string;
+  name: string;
+  slots: string[];
 }
 
-const AppointmentCards = ({selectedDate}:DateProps) => {
-    const [apointmentsData, setAppointmentsData]= useState<AppointmentsProps[]>([])
-console.log(selectedDate)
-    useEffect(() => {
-    fetch("/data/appointmentsSlot.json")
-    .then(res=> res.json())
-    .then(data=>{
-        console.log(data)
-        setAppointmentsData(data)
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-    }, [])
-    
+const AppointmentCards = ({ selectedDate }: DateProps) => {
+  const [apointmentsData, setAppointmentsData] = useState<AppointmentsProps[]>(
+    []
+  );
+  console.log(selectedDate);
+  useEffect(() => {
+    fetch("http://localhost:8000/appointments")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAppointmentsData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
-        <div>
-        {selectedDate ? <p className='text-center text-xl'>You have selected your Appointment on <span className='font-bold'>{format(selectedDate, 'PP')}</span></p>: <p className='text-center text-xl '>You have No Appoinmetns</p>}
-    </div>
-        <div className='grid grid-cols-1 md:grid-cols-2  text-center col-start-2 mt-16 gap-7 mx-5'>
-{
-    apointmentsData.map(data=>{
-        return(
-            <div key={data.id as React.Key} className='border mx-12 shadow rounded'>
-                <AppointmentSlot slotData={data} selectedDate={{selectedDate}}/>
+      <div>
+        {selectedDate ? (
+          <p className="text-center text-xl">
+            You have selected your Appointment on{" "}
+            <span className="font-bold">{format(selectedDate, "PP")}</span>
+          </p>
+        ) : (
+          <p className="text-center text-xl ">You have No Appoinmetns</p>
+        )}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2  text-center col-start-2 mt-16 gap-7 mx-5">
+        {apointmentsData.map((data) => {
+          return (
+            <div
+              key={data.id as React.Key}
+              className="border mx-12 shadow rounded"
+            >
+              <AppointmentSlot
+                slotData={data}
+                selectedDate={{ selectedDate }}
+              />
             </div>
-        )})
-}
-        </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AppointmentCards
+export default AppointmentCards;
