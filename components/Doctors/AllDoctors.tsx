6afa58048778d5e743a2ad26ebbda1710c612dca
unknown <alaminkhan6203@gmail.com>
2023-03-02 01:41:24 +0600

    adding caause deployment done

--- components/Doctors/AllDoctors.tsx
+++ components/Doctors/AllDoctors.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Doctor from "./Doctor";
 import Lottie from "lottie-react";
 import loader from "../../public/data/Heart-2.json";
+import hero from "../../public/data/Comp 1.json";
 
 export interface DoctorsProps {
   _id: string;
@@ -35,6 +36,18 @@ const AllDoctors = () => {
   }
   return (
     <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className=" w-[300px] h-[300px]">
+          <Lottie animationData={hero} loop={true} />
+          <br />
+          <p>
+            Meet your{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 uppercase  font-bold">
+              Heroes
+            </span>
+          </p>
+        </div>
+      </div>
       {doctors?.map((doctor: DoctorsProps) => {
         return (
           <div key={doctor._id}>
