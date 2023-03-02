1161dc9a3a5ce1041d3b18bffac5a01592713fac
unknown <alaminkhan6203@gmail.com>
2023-03-02 21:56:57 +0600

    again designing

--- components/HomePage/TopBanner.tsx
+++ components/HomePage/TopBanner.tsx
@@ -27,7 +27,8 @@ const TopBanner = (props: Props) => {
           height={600}
         ></Image>
       </div>
-      <div className=" flex flex-col custom-img bg-cover justify-center items-center p-5 ">
+      <div className=" flex flex-col  justify-center items-center p-5 ">
+        <div className="custom-img bg-cover"></div>
         <h1 className="font-extrabold text-5xl ">
           Experience the{" "}
           <span className="text-transparent   bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 uppercase  font-bold">
