915a0131ff533651e524c4da705ed26897c42e36
unknown <alaminkhan6203@gmail.com>
2023-03-03 21:34:11 +0600

    something new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,8 +51,8 @@ const Navbar = () => {
     : menuItems;
 
   return (
-    <div className="relative bg-green-300">
-      <div className=" hidden md:block fixed m-0 bg-green-300 min-w-full py-5 px-5 z-40">
+    <div className="relative ">
+      <div className=" hidden md:block fixed m-0 bg-green-200 min-w-full py-5 px-5 z-40">
         <div className="grid grid-cols-2 ">
           <p className="tracking-widest">THERAPEUTIC TOUCHPOINT</p>
           <div className="flex justify-around">
